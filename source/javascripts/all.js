4c336f8a6fb2ec28ebc8291e79b9e5168eb9e93f
zhangxiaohua <445679586@qq.com>
2013-12-14 16:56:17 +0800

    3张banner图

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -12,6 +12,15 @@ $(document).ready(function() {
         current_nav_menu.addClass('current');
     });
 
+    // 首页banner的切换
+    var banners = $('.banners li');
+    $('.banners').click(function(){
+        var now = banners.index($('.banners li:visible'));
+        var next = now + 1 >= banners.length ? 0 : now + 1;
+        banners.hide().eq(next).show();
+    });
+
     // RWD javascript
+
 
 });
